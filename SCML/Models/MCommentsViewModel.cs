83297515157d44b390805abb73e60ab1097d998c
sarwarmdgolam <sarwarmdgolam@yahoo.com>
2016-05-02 12:48:43 +0600

    Create MCommentsViewModel.cs

---
+++ SCML/Models/MCommentsViewModel.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SCML.Models
+{
+    public class MCommentsViewModel
+    {
+        public IEnumerable<Content> MComments { get; set; }
+    }
+}
