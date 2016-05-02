cfd8841302685978b6394577c5a883ab333d25f6
sarwarmdgolam <sarwarmdgolam@yahoo.com>
2016-05-02 12:49:06 +0600

    Create MessagesViewModel.cs

---
+++ SCML/Models/MessagesViewModel.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SCML.Models
+{
+    public class MessagesViewModel
+    {
+        public Content Messages { get; set; }
+    }
+}
