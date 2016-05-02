931b91bc0ad44c098b00e24b01bb0f10b890f8df
sarwarmdgolam <sarwarmdgolam@yahoo.com>
2016-05-02 12:48:03 +0600

    Create LNewsViewModel.cs

---
+++ SCML/Models/LNewsViewModel.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SCML.Models
+{
+    public class LNewsViewModel
+    {
+        public IEnumerable<Content> LNews { get; set; }
+    }
+}
